ee470bb29f97fdd8ef8be5dac0d9eb359f8483f0
Stefan Siegl <stesie@brokenpipe.de>
2008-06-07 15:23:10 +0200

    import chip low-level routines, initialize the chip correctly and put into rx mode.

--- h3600_rfm12.c
+++ h3600_rfm12.c
@@ -22,12 +22,123 @@
 #include <linux/kmod.h>
 #include <linux/kernel.h>
 
-#define MODULE_NAME "h3600_rfm12"
+#include <asm/arch-sa1100/h3600_hal.h>
+#include "h3600_rfm12.h"
 
+/*
+ * RFM12 Chip low-level functions
+ */
+#define chip_trans(a)  h3600_rfm12(a)
+
+static enum {
+	CHIP_IDLE,
+	CHIP_RX,
+	CHIP_RX_DATA,
+
+	CHIP_TX,
+	CHIP_TX_PREAMBLE_1,
+	CHIP_TX_PREAMBLE_2,
+	CHIP_TX_PREFIX_1,
+	CHIP_TX_PREFIX_2,
+	CHIP_TX_SIZE,
+	CHIP_TX_DATA,
+	CHIP_TX_DATAEND,
+	CHIP_TX_SUFFIX_1,
+	CHIP_TX_SUFFIX_2,
+	CHIP_TX_END,
+} chip_status;			/* Locked by chip_lock. */
+
+static struct sk_buff *rx_packet, *tx_packet;
+static int packet_len;
+static int packet_index;
+
+
+static void
+chip_setbandwidth(u8 bandwidth, u8 gain, u8 drssi)
+{
+	chip_trans (0x9400 | ((bandwidth & 7) << 5)
+		    | ((gain & 3) << 3) | (drssi & 7));
+}
+
+
+static void
+chip_setfreq(u16 freq)
+{	
+	if (freq < 96)		/* 430,2400MHz */
+		freq = 96;
+
+	else if (freq > 3903)	/* 439,7575MHz */
+		freq = 3903;
+
+	chip_trans (0xA000 | freq);
+}
+
+
+static void
+chip_setbaud(u16 baud)
+{
+	if (baud < 663)
+		return;
+
+	/* Baudrate = 344827,58621 / (R + 1) / (1 + CS * 7) */
+	if (baud < 5400)
+		chip_trans (0xC680 | ((43104 / baud) - 1));
+	else
+		chip_trans (0xC600 | ((344828UL / baud) - 1));
+}
+
+
+static void
+chip_setpower(u8 power, u8 mod)
+{	
+	chip_trans (0x9800 | (power & 7) | ((mod & 15) << 4));
+}
+
+
+static void
+chip_init (void)
+{
+	chip_trans (0xC0E0);	/* AVR CLK: 10MHz */
+	chip_trans (0x80D7);	/* Enable FIFO */
+	chip_trans (0xC2AB);	/* Data Filter: internal */
+	chip_trans (0xCA81);	/* Set FIFO mode */
+	chip_trans (0xE000);	/* disable wakeuptimer */
+	chip_trans (0xC800);	/* disable low duty cycle */
+	chip_trans (0xC4F7);	/* autotuning: -10kHz...+7,5kHz */
+	chip_trans (0x0000);
+
+	chip_setfreq (RFM12FREQ (433.92));
+	chip_setbandwidth (5, 1, 4);
+	chip_setbaud (8620);
+	chip_setpower (0, 2);
+}
+
+
+static void
+chip_rxstart (void)
+{
+	chip_trans (0x82C8);	/* RX on */
+	chip_trans (0xCA81);	/* set FIFO mode */
+	chip_trans (0xCA83);	/* enable FIFO */
+
+	chip_status = CHIP_RX;
+}
+
+
+/*
+ * Module related functions, glueing everything together.
+ */
 int
 init_module (void)
 {
 	printk (KERN_INFO MODULE_NAME ": init_module called.\n");
+
+	chip_init ();
+	chip_rxstart ();
+
+	/* initiate dummy read */
+	h3600_rfm12 (0x0000);
+
 	return 0;
 }
 
---
+++ h3600_rfm12.h
@@ -0,0 +1,77 @@
+/*
+ * rfm12.h - GPIO interface driver for RFM12 low-cost FSK transmitter
+ *
+ * Copyright (C) 2008 Stefan Siegl <stesie@brokenpipe.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef RFM12_H
+#define RFM12_H
+
+#include <linux/skbuff.h>
+
+#define MODULE_NAME "h3600_rfm12"
+
+/* Replace DEBUG by printk(a) to enable debug output. */
+#define DEBUG(a...)   printk(MODULE_NAME ": " a)
+
+#define STATS ((struct net_device_stats *) rfm12_dev.priv)
+
+#define RxBW400		1
+#define RxBW340		2
+#define RxBW270		3
+#define RxBW200		4
+#define RxBW134		5
+#define RxBW67		6
+
+#define TxBW15		0
+#define TxBW30		1
+#define TxBW45		2
+#define TxBW60		3
+#define TxBW75		4
+#define TxBW90		5
+#define TxBW105		6
+#define TxBW120		7
+
+#define LNA_0		0
+#define LNA_6		1
+#define LNA_14		2
+#define LNA_20		3
+
+#define RSSI_103	0
+#define RSSI_97		1
+#define RSSI_91		2
+#define RSSI_85		3
+#define RSSI_79		4
+#define RSSI_73		5
+#define RSSI_67		6
+#define	RSSI_61		7
+
+#define PWRdB_0		0
+#define PWRdB_3		1
+#define PWRdB_6		2
+#define PWRdB_9		3
+#define PWRdB_12	4
+#define PWRdB_15	5
+#define PWRdB_18	6
+#define PWRdB_21	7
+
+#define RFM12TxBDW(kfrq)	((u8) (kfrq / 15) - 1)
+#define RFM12FREQ(freq)	        ((freq - 430.0) / 0.0025)	
+
+
+
+#endif	/* RFM12_H */
